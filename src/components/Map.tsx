0c26539d3c8f8c93d5b6328cbc6ca6eadd736450
David Abell <79927957+david-abell@users.noreply.github.com>
2023-04-12 15:37:00 +0100

    add leaflet

---
+++ src/components/Map.tsx
@@ -0,0 +1,30 @@
+"use-client";
+
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
+function Map() {
+  return (
+    <div>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        className="relative h-[100vmin] w-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
+"use-client";
+
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import dynamic from "next/dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const Map = dynamic(() => import("@/components/Map"), {
+    ssr: false,
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className=" w-full max-w-5xl items-center justify-between lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className=" w-full max-w-5xl items-center justify-between">
         <h1>H1 Title</h1>
+        <Map />
       </div>
     </main>
   );

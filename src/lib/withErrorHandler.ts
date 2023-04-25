a967f42d6a317bc195206af40bad96b2d64309cc
David Abell <79927957+david-abell@users.noreply.github.com>
2023-04-25 18:45:09 +0100

    convert error handler to function

--- src/lib/withErrorHandler.ts
+++ src/lib/withErrorHandler.ts
@@ -1,18 +1,18 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { ApiError } from "next/dist/server/api-utils";
 
-const withErrorHandler =
-  (handler: NextApiHandler) =>
-  async (req: NextApiRequest, res: NextApiResponse) => {
+function withErrorHandler(handler: NextApiHandler) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       return handler(req, res);
     } catch (error) {
-      if (error instanceof ApiError) {
-        res.status(error.statusCode).send(error.message);
+      if (error instanceof Error) {
+        res.status(400).send(error.message);
       } else {
         res.status(500).send({ error: "Internal server error" });
       }
     }
   };
+}
 
 export default withErrorHandler;

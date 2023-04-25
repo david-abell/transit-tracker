d2703bdb3a8f2fe21b009a1472f22e42c41ae58e
David Abell <79927957+david-abell@users.noreply.github.com>
2023-04-25 18:48:08 +0100

    add calendar api

---
+++ src/pages/api/gtfs/static/calendar.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/db";
+import withErrorHandler from "@/lib/withErrorHandler";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Calendar } from "@prisma/client";
+import { ApiError } from "next/dist/server/api-utils";
+
+export type CalendarAPIResponse = Calendar;
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<CalendarAPIResponse>
+) {
+  const { serviceId } = req.query;
+
+  if (!serviceId || typeof serviceId !== "string") {
+    return res.end();
+  }
+
+  const service = await prisma.calendar.findFirst({ where: { serviceId } });
+
+  if (!service) {
+    throw new ApiError(404, "No service found");
+  }
+
+  return res.json(service);
+}
+
+export default withErrorHandler(handler);

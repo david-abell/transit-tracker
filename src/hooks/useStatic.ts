3a2f3b2f32d2fe62231ae8ffc49325ecf8f54936
David Abell <79927957+david-abell@users.noreply.github.com>
2023-04-25 18:44:51 +0100

    fix stoptimes, add trips and stoptimes by id

--- src/hooks/useStatic.ts
+++ src/hooks/useStatic.ts
@@ -1,9 +1,12 @@
 import useSWR from "swr";
 import { useState } from "react";
 import { Route, Shape, StopTime, Trip } from "@prisma/client";
+import { format } from "date-fns";
 
 import { StaticAPIResponse } from "@/pages/api/gtfs/static/static";
 import { ShapeAPIResponse } from "@/pages/api/gtfs/static/shape";
+import StopId from "@/pages/api/gtfs/static/stop-times/[stopId]";
+import { dateToStopTimeString } from "@/lib/timeHelpers";
 
 const fetcher = async (input: RequestInfo, init: RequestInit) => {
   try {
@@ -18,11 +21,11 @@ const fetcher = async (input: RequestInfo, init: RequestInit) => {
 
 type Props = {
   routeQuery?: string;
-  tripId?: string;
   shapeId?: string;
+  dateTime?: Date;
 };
 
-function useStatic({ routeQuery, tripId, shapeId }: Props) {
+function useStatic({ routeQuery, shapeId, dateTime }: Props) {
   const { data: staticData } = useSWR<StaticAPIResponse>(
     () =>
       !!routeQuery
@@ -35,13 +38,43 @@ function useStatic({ routeQuery, tripId, shapeId }: Props) {
 
   const { route, stops, trips } = staticData || {};
 
+  const tripsById = new Map(
+    trips?.map((data) => {
+      const { tripId } = data;
+      return [tripId, data];
+    })
+  );
+
+  const stopsById = new Map(
+    stops?.map((data) => {
+      const { stopId } = data;
+      return [stopId, data];
+    })
+  );
+
   const { data: stopTimes } = useSWR<StopTime[]>(
     () =>
-      !!tripId
-        ? `/api/gtfs/static/stop-times/?${encodeURIComponent(tripId)}}`
+      !!route && dateTime
+        ? `/api/gtfs/static/stop-times?${new URLSearchParams({
+            routeId: route.routeId,
+            departureTime: dateToStopTimeString(dateTime),
+            utc: dateTime.toISOString(),
+          })}`
         : null,
     fetcher
   );
+
+  const stopTimesByTripId =
+    stopTimes &&
+    stopTimes?.reduce((acc, val) => {
+      const { tripId } = val;
+      if (acc.has(tripId)) {
+        acc.set(tripId, acc.get(tripId).concat(val));
+      } else {
+        acc.set(tripId, [val]);
+      }
+      return acc;
+    }, new Map());
 
   const { data: shape } = useSWR<ShapeAPIResponse>(
     () =>
@@ -58,7 +91,10 @@ function useStatic({ routeQuery, tripId, shapeId }: Props) {
     shape,
     stops,
     trips,
+    tripsById,
     stopTimes,
+    stopTimesByTripId,
+    stopsById,
   };
 }
 

3b4eaf9684f760a12997bb2101093e65cedeabb1
David Abell <79927957+david-abell@users.noreply.github.com>
2023-05-04 18:01:34 +0100

    style and add arrow keyboard events

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -18,36 +18,123 @@ function SearchInput({ setSelectedRoute }: Props) {
     setRouteName("");
   };
 
+  // Move focus to/from search input into next/previous <li><button>This child button</button></li>
+  const handleArrowKeydown = (
+    e: React.KeyboardEvent<HTMLButtonElement | HTMLInputElement>
+  ) => {
+    if (e.key === "ArrowDown" && e.currentTarget instanceof HTMLInputElement) {
+      (
+        e.currentTarget.nextElementSibling?.firstChild
+          ?.firstChild as HTMLElement
+      ).focus();
+    } else if (
+      e.key === "ArrowDown" &&
+      e.currentTarget.parentElement?.nextElementSibling?.firstChild instanceof
+        HTMLButtonElement
+    ) {
+      (
+        e.currentTarget.parentElement?.nextElementSibling
+          ?.firstChild as HTMLElement
+      ).focus();
+    }
+    if (
+      e.key === "ArrowUp" &&
+      e.currentTarget.parentElement?.parentElement?.firstElementChild ===
+        e.currentTarget.parentElement
+    ) {
+      console.log(
+        e.currentTarget.parentElement?.parentElement.previousElementSibling
+      );
+      (
+        e.currentTarget.parentElement?.parentElement
+          ?.previousElementSibling as HTMLElement
+      ).focus();
+    } else if (
+      e.key === "ArrowUp" &&
+      e.currentTarget instanceof HTMLButtonElement &&
+      e.currentTarget.parentElement?.previousElementSibling?.firstChild
+    ) {
+      (
+        e.currentTarget.parentElement?.previousElementSibling
+          ?.firstChild as HTMLElement
+      ).focus();
+    }
+  };
+
   return (
     <div className="relative">
-      <label htmlFor="route-search" className="p-2">
-        Search for a Route:{" "}
-      </label>
-      <input
-        type={"search"}
-        value={routeName}
-        onChange={({ currentTarget }) => setRouteName(currentTarget.value)}
-        id="route-search"
-        className="p-2"
-      />
-      {!!routes && (
-        <ul className="absolute left-0 top-full z-[2000] bg-white p-2">
-          {routes.map((route) => {
-            const { routeId, routeLongName, routeShortName } = route;
-            return (
-              <li key={routeId}>
-                <button
-                  onClick={(e) => handleSetSelectedRoute(e, route)}
-                  className="w-full text-left hover:bg-slate-200"
-                >
-                  <strong>{routeShortName}: </strong>
-                  {routeLongName}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+      <form className="flex items-center">
+        <label htmlFor="route-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative w-full">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg
+              aria-hidden="true"
+              className="h-5 w-5 text-gray-500 dark:text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            id="route-search"
+            className="block w-full rounded-b-lg rounded-t-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus-within:rounded-b-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            placeholder="Search for bus or train route"
+            required
+            type="search"
+            value={routeName}
+            onChange={({ currentTarget }) => setRouteName(currentTarget.value)}
+            autoComplete="off"
+            onKeyDown={(e) => handleArrowKeydown(e)}
+          />
+          {!!routes && (
+            <ul className="border-gray-300text-md absolute left-0 top-full block w-full rounded-b-lg border bg-gray-50 p-2.5 pl-10 text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+              {routes.map((route) => {
+                const { routeId, routeLongName, routeShortName } = route;
+                return (
+                  <li key={routeId} className="pb-2">
+                    <button
+                      onClick={(e) => handleSetSelectedRoute(e, route)}
+                      className="w-full text-left hover:bg-slate-200"
+                      onKeyDown={(e) => handleArrowKeydown(e)}
+                    >
+                      <strong>{routeShortName}: </strong>
+                      {routeLongName}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="relative ml-2 rounded-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <svg
+            className="h-5 w-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+          <span className="sr-only">Search</span>
+        </button>
+      </form>
     </div>
   );
 }

e809a65d1398bc2e2c59c573c33d7a4246dec5bb
David Abell <79927957+david-abell@users.noreply.github.com>
2023-05-05 13:40:33 +0100

    layout cleanup

--- src/components/DateTimeSelect.tsx
+++ src/components/DateTimeSelect.tsx
@@ -13,15 +13,15 @@ function DateTimeSelect({ selectedDateTime, setSelectedDateTime }: Props) {
   };
 
   return (
-    <div className="flex flex-row items-center justify-center gap-10 p-4">
+    <div className="flex flex-col items-center justify-center gap-4 p-4">
       <label htmlFor="date-time-select">When are you traveling? </label>
       <input
         type="datetime-local"
         value={selectedDateTime}
         onChange={(e) => handleSetSelectedDateTime(e)}
         id="date-time-select"
-        className="rounded-b-lg rounded-t-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 
-         dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+        className="w-full rounded-b-lg rounded-t-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 
+         focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
       ></input>
     </div>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -67,11 +67,9 @@ function SearchInput({ setSelectedRoute }: Props) {
 
   return (
     <div className="relative">
-      <form className="flex items-center">
-        <label htmlFor="route-search" className="sr-only">
-          Search
-        </label>
-        <div className="relative w-full">
+      <form className="flex flex-col items-center justify-center gap-4 p-4 text-center">
+        <label htmlFor="route-search">Search for a travel route</label>
+        <div className="relative flex w-full">
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
             <svg
               aria-hidden="true"
@@ -87,10 +85,13 @@ function SearchInput({ setSelectedRoute }: Props) {
               ></path>
             </svg>
           </div>
+
           <input
             id="route-search"
-            className="block w-full rounded-b-lg rounded-t-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus-within:rounded-b-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="Search for bus or train route"
+            className="inline-block flex-1 rounded-b-lg rounded-t-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900
+               focus-within:rounded-b-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400
+               dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            placeholder="Search for a bus or train route"
             required
             type="search"
             value={routeName}
@@ -99,7 +100,10 @@ function SearchInput({ setSelectedRoute }: Props) {
             onKeyDown={(e) => handleSearchKeydown(e)}
           />
           {!!routes && (
-            <ul className="border-gray-300text-md absolute left-0 top-full block w-full rounded-b-lg border bg-gray-50 p-2.5 pl-10 text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <ul
+              className="border-gray-300text-md absolute left-0 top-full block w-full rounded-b-lg border bg-gray-50 p-2.5 pl-10 text-gray-900 focus:border-blue-500
+               focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            >
               {routes.map((route) => {
                 const { routeId, routeLongName, routeShortName } = route;
                 return (
@@ -117,27 +121,28 @@ function SearchInput({ setSelectedRoute }: Props) {
               })}
             </ul>
           )}
-        </div>
-        <button
-          type="submit"
-          className="relative ml-2 rounded-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          <svg
-            className="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+          <button
+            type="submit"
+            className="ml-2 rounded-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 
+              focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-          <span className="sr-only">Search</span>
-        </button>
+            <svg
+              className="h-5 w-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+        </div>
       </form>
     </div>
   );

d2c5fbddb4c5383c0735f162a2346be986e50cbd
David Abell <79927957+david-abell@users.noreply.github.com>
2023-05-05 12:18:08 +0100

    remove unused handler, add styles

--- src/components/DateTimeSelect.tsx
+++ src/components/DateTimeSelect.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 
 type Props = {
   selectedDateTime: string;
@@ -10,21 +10,18 @@ function DateTimeSelect({ selectedDateTime, setSelectedDateTime }: Props) {
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
     setSelectedDateTime(e.target.value);
-    console.log(e.target.value);
-  };
-  const handleSetSelectedTime = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedDateTime(e.target.value);
-    console.log(e.target.value);
   };
 
   return (
     <div className="flex flex-row items-center justify-center gap-10 p-4">
-      <label htmlFor="date-time-select">Travel time: </label>
+      <label htmlFor="date-time-select">When are you traveling? </label>
       <input
         type="datetime-local"
         value={selectedDateTime}
         onChange={(e) => handleSetSelectedDateTime(e)}
         id="date-time-select"
+        className="rounded-b-lg rounded-t-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 
+         dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
       ></input>
     </div>
   );

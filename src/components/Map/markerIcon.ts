f515b09e1853d427f5a6a115c9062ab8da821f75
David Abell <79927957+david-abell@users.noreply.github.com>
2023-05-10 15:00:56 +0100

    update icon to bus

--- src/components/Map/markerIcon.ts
+++ src/components/Map/markerIcon.ts
@@ -13,9 +13,10 @@ type Color =
 
 export function markerIcon(color: Color) {
   return new Icon({
-    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
-    shadowUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    iconUrl: new URL(`
+    data:image/svg+xml,%0A%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12'%3E%3Cpath fill='${color}' d='M3 1 2 2v8h.66s.1 1 .8 1c.7 0 .79-1 .79-1h3.5s.11 1 .78 1c.82 0 .81-1 .81-1H10V2L9 1zm1 1h4v1H4zM3 4h6v3H3zm0 4h1v1H3zm5 0h1v1H8z'/%3E%3C/svg%3E`)
+      .href,
+    shadowUrl: "",
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],

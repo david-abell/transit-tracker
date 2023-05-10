17c2f73dbf7791d941589a246ca8e6a230adc00b
David Abell <79927957+david-abell@users.noreply.github.com>
2023-05-10 11:13:58 +0100

    add styles for modal

--- src/components/TripSelect.tsx
+++ src/components/TripSelect.tsx
@@ -12,35 +12,29 @@ function TripSelect({
   setSelectedTripId,
   tripsById,
 }: Props) {
-  if (stopStopTimes.length >= 2) {
-    return (
-      <div>
-        <label htmlFor="trip-select">
-          Pick a trip
-          <select
-            className="w-36"
-            onChange={({ currentTarget }) =>
-              setSelectedTripId(currentTarget.value)
-            }
-            id="trip-select"
-          >
-            {stopStopTimes.map(({ tripId, departureTime }) => (
-              <option value={tripId} key={tripId}>
-                {tripsById.get(tripId)?.tripHeadsign}: {departureTime}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <span>Selected trip: </span>
-      <span>
-        {tripsById.get(stopStopTimes?.[0]?.tripId)?.tripHeadsign || ""}
-      </span>
+    <div className="w-full rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
+      <label htmlFor="trip-select" className="sr-only">
+        Pick a trip
+      </label>
+      <ul id="trip-select" className="max-w-full">
+        {stopStopTimes.map(({ tripId, departureTime }) => (
+          <li value={tripId} key={tripId}>
+            <button
+              type="button"
+              onClick={({ currentTarget }) =>
+                setSelectedTripId(currentTarget.value)
+              }
+              className="w-full cursor-pointer border-b border-gray-200 px-4 py-2 text-left font-medium 
+                hover:bg-gray-100 hover:text-blue-700 focus:text-blue-700 focus:outline-none focus:ring-2 
+                focus:ring-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white 
+                dark:focus:text-white dark:focus:ring-gray-500"
+            >
+              {tripsById.get(tripId)?.tripHeadsign}: {departureTime}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

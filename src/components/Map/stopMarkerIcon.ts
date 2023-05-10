793962e738df086d2af73cf988a68d4f0de2813e
David Abell <79927957+david-abell@users.noreply.github.com>
2023-05-10 20:21:46 +0100

    resize icon

--- src/components/Map/stopMarkerIcon.ts
+++ src/components/Map/stopMarkerIcon.ts
@@ -19,8 +19,8 @@ export function stopMarkerIcon(isUpcoming: boolean, isCurrent = false) {
     }' d='M3 1 2 2v8h.66s.1 1 .8 1c.7 0 .79-1 .79-1h3.5s.11 1 .78 1c.82 0 .81-1 .81-1H10V2L9 1zm1 1h4v1H4zM3 4h6v3H3zm0 4h1v1H3zm5 0h1v1H8z'/%3E%3C/svg%3E`)
       .href,
     shadowUrl: "",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
+    iconSize: [28, 28],
+    iconAnchor: [14, 14],
     popupAnchor: [1, -34],
     shadowSize: [41, 41],
   });

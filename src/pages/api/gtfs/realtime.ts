b01ef30aa858fbcf0793f01d262e3ccf1f2a948c
David Abell <79927957+david-abell@users.noreply.github.com>
2023-05-16 14:36:56 +0100

    add realtime data fetching

--- src/pages/api/gtfs/realtime.ts
+++ src/pages/api/gtfs/realtime.ts
@@ -3,9 +3,32 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import gtfsRealtime from "@/testdata/gtfsrealtime2.json";
 import { GTFSResponse } from "@/types/realtime";
 
-export default function handler(
+const API_KEY = process.env.NTA_REALTIME_API_KEY;
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<GTFSResponse>
 ) {
-  res.status(200).json(gtfsRealtime as unknown as GTFSResponse);
+  try {
+    console.log("realtime called:", new Date().toLocaleString());
+    const response = await fetch(
+      "https://api.nationaltransport.ie/gtfsr/v2/gtfsr?format=json",
+      {
+        method: "GET",
+        // Request headers
+        headers: {
+          "x-api-key": API_KEY as string,
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`statusText: ${response.statusText}`);
+    }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error("api error", err);
+    }
+  }
 }

898d258fafa10c998029397049af51e53c1d0b54
David Abell <79927957+david-abell@users.noreply.github.com>
2023-05-17 13:38:38 +0100

    copy leaflet css to styles dir for custom tooltips

--- src/components/Map/index.ts
+++ src/components/Map/index.ts
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-import "leaflet/dist/leaflet.css";
 
 // exporting a dynamic import resolves render flashes on load
 export default dynamic(() => import("./MapWithContent"), {

4e2f88f4e77619502b5f0665ef030d35aa1daf4d
David Abell <79927957+david-abell@users.noreply.github.com>
2023-06-12 12:37:36 +0100

    add classname, fix rounded input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -6,11 +6,11 @@ import { useRouter } from "next/router";
 
 type Props = {
   selectedRoute: Route | undefined;
+  className?: string;
 };
 
-function SearchInput({ selectedRoute }: Props) {
+function SearchInput({ selectedRoute, className = "" }: Props) {
   const router = useRouter();
-  // const { routeId } = router.query;
   const [routeName, setRouteName] = useState("");
   const { routes } = useRoute(routeName);
   const formRef = useRef<HTMLFormElement>(null);
@@ -67,96 +67,98 @@ function SearchInput({ selectedRoute }: Props) {
   // };
 
   return (
-    <form
-      ref={formRef}
-      className="flex w-full flex-1 flex-col items-center justify-center text-center md:min-w-[28rem]"
-      // onSubmit={handleOnSubmit}
-    >
-      <label htmlFor="route-search" className="sr-only">
-        Search for a travel route
-      </label>
-      <div className="relative flex w-full">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2 md:pl-2.5">
-          <svg
-            aria-hidden="true"
-            className="h-5 w-5 text-gray-500 dark:text-gray-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </div>
+    <div className={className}>
+      <form
+        ref={formRef}
+        className="flex w-full flex-1 flex-col items-center justify-center text-center md:min-w-[28rem]"
+        // onSubmit={handleOnSubmit}
+      >
+        <label htmlFor="route-search" className="sr-only">
+          Search for a travel route
+        </label>
+        <div className="relative flex w-full">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2 md:pl-2.5">
+            <svg
+              aria-hidden="true"
+              className="h-5 w-5 text-gray-500 dark:text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </div>
 
-        <input
-          id="route-search"
-          className={`text-md inline-block flex-1 appearance-none truncate rounded-none rounded-bl-lg rounded-tl-lg border border-gray-300 bg-gray-50 p-2.5 pl-8
+          <input
+            id="route-search"
+            className={`text-md inline-block flex-1 appearance-none truncate rounded-none rounded-bl-lg rounded-tl-lg border border-gray-300 bg-gray-50 p-2.5 pl-8
                text-gray-900 focus-within:rounded-bl-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white
                dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 md:pl-10
-               ${routeName ? "rounded-b-none" : ""}`}
-          placeholder={
-            selectedRoute?.routeLongName || "Search for a bus or train route"
-          }
-          type="search"
-          value={routeName}
-          onChange={({ currentTarget }) => setRouteName(currentTarget.value)}
-          autoComplete="off"
-          aria-autocomplete="list"
-          onKeyDown={(e) => handleSearchKeydown(e)}
-        />
-        {!!routes && (
-          <ul
-            className="text-md absolute left-0 top-full z-[1200] block w-full rounded-b-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 pr-10 text-gray-900
+               ${routeName ? "rounded-bl-none" : ""}`}
+            placeholder={
+              selectedRoute?.routeLongName || "Search for a bus or train route"
+            }
+            type="search"
+            value={routeName}
+            onChange={({ currentTarget }) => setRouteName(currentTarget.value)}
+            autoComplete="off"
+            aria-autocomplete="list"
+            onKeyDown={(e) => handleSearchKeydown(e)}
+          />
+          {!!routes && (
+            <ul
+              className="text-md absolute left-0 top-full z-[1200] block w-full rounded-b-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 pr-10 text-gray-900
                focus:border-blue-500  focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-          >
-            {routes.map((route) => {
-              const { routeId, routeLongName, routeShortName } = route;
-              return (
-                <li key={routeId} className="pb-2">
-                  <button
-                    type="button"
-                    onClick={(e) => handleSetSelectedRoute(e, route)}
-                    className="w-full text-left hover:bg-slate-200"
-                    onKeyDown={(e) => handleSearchKeydown(e)}
-                  >
-                    <strong>{routeShortName}: </strong>
-                    {routeLongName}
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-        <button
-          // onClick={handleOnSubmit}
-          onKeyDown={(e) => handleSearchKeydown(e)}
-          type="submit"
-          className={`text-md flex w-12 justify-center rounded-br-lg rounded-tr-lg border border-blue-700 bg-blue-700 p-2.5 font-medium text-white hover:bg-blue-800 
+            >
+              {routes.map((route) => {
+                const { routeId, routeLongName, routeShortName } = route;
+                return (
+                  <li key={routeId} className="pb-2">
+                    <button
+                      type="button"
+                      onClick={(e) => handleSetSelectedRoute(e, route)}
+                      className="w-full text-left hover:bg-slate-200"
+                      onKeyDown={(e) => handleSearchKeydown(e)}
+                    >
+                      <strong>{routeShortName}: </strong>
+                      {routeLongName}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+          <button
+            // onClick={handleOnSubmit}
+            onKeyDown={(e) => handleSearchKeydown(e)}
+            type="submit"
+            className={`text-md flex w-12 justify-center rounded-br-lg rounded-tr-lg border border-blue-700 bg-blue-700 p-2.5 font-medium text-white hover:bg-blue-800 
               focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
               ${routeName ? "rounded-br-none" : ""} `}
-        >
-          <svg
-            className="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-          <span className="sr-only">Search</span>
-        </button>
-      </div>
-    </form>
+            <svg
+              className="h-5 w-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 

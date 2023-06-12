b20b9e408789470f7c865452193907db25b2a55a
David Abell <79927957+david-abell@users.noreply.github.com>
2023-06-12 12:45:58 +0100

    remove unused leaflet icon fix

--- src/components/Map/TileLayerWrapper.tsx
+++ src/components/Map/TileLayerWrapper.tsx
@@ -1,23 +1,9 @@
 "use-client";
 
 import { MapContainer, TileLayer } from "react-leaflet";
-import Leaflet from "leaflet";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 
 function TileLayerWrapper({ children }: { children: ReactNode }) {
-  // Fix leaflet icons not importing
-  useEffect(() => {
-    (async function init() {
-      // @ts-ignore
-      delete Leaflet.Icon.Default.prototype._getIconUrl;
-      Leaflet.Icon.Default.mergeOptions({
-        iconRetinaUrl: "leaflet/images/marker-icon-2x.png",
-        iconUrl: "leaflet/images/marker-icon.png",
-        shadowUrl: "leaflet/images/marker-shadow.png",
-      });
-    })();
-  }, []);
-
   return (
     <MapContainer
       center={[51.9081690653422, -8.41944955885327]}

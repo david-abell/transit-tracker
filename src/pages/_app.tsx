3742c567e4ae864f4f050ef31a9675eb3695f804
David Abell <79927957+david-abell@users.noreply.github.com>
2023-06-14 15:50:08 +0100

    add error boundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import ErrorBoundary from "@/components/ErrorBoundary";
 import "@/styles/globals.css";
 import "@/styles/leaflet.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }

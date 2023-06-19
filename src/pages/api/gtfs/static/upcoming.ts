de780e1f3c7c17b01a1289c11528497a72f1ea39
David Abell <79927957+david-abell@users.noreply.github.com>
2023-06-19 19:18:42 +0100

    fix: extremely slow prisma stoptimes query

--- src/pages/api/gtfs/static/upcoming.ts
+++ src/pages/api/gtfs/static/upcoming.ts
@@ -6,7 +6,9 @@ import { Route, StopTime, Trip } from "@prisma/client";
 import { ApiError } from "next/dist/server/api-utils";
 import { differenceInSeconds, parseISO, startOfDay } from "date-fns";
 import { getCalendarDate, getDayString } from "@/lib/timeHelpers";
-import { scheduledService } from "@/lib/api/static/consts";
+import { scheduledService, serviceException } from "@/lib/api/static/consts";
+import { calendarDayColumns } from "./trips";
+import camelcaseKeys from "camelcase-keys";
 
 export type TripsByStopIdAPIResponse = {
   routes: Route[];
@@ -15,8 +17,6 @@ export type TripsByStopIdAPIResponse = {
 };
 
 // type NumericalString = `${number}`;
-
-const pageCount = 12;
 
 async function handler(
   req: NextApiRequest,
@@ -37,38 +37,40 @@ async function handler(
   const startOfDate = startOfDay(date);
   const departureTimeInSeconds = differenceInSeconds(date, startOfDate);
 
-  const stopTimes = await prisma.stopTime.findMany({
-    skip: Number(page) * pageCount,
-    take: 12,
-    where: {
-      AND: [
-        { stopId: stopId },
-        { arrivalTimestamp: { gte: departureTimeInSeconds } },
-        {
-          NOT: {
-            trip: {
-              calendar: {
-                [dayOfWeek]: { equals: scheduledService.isFalse },
-              },
-            },
-          },
-          OR: [
-            {
-              trip: {
-                calendar: { calendarDate: { some: { date: dateOfYear } } },
-              },
-            },
-            {
-              trip: {
-                calendar: { [dayOfWeek]: { equals: scheduledService.isTrue } },
-              },
-            },
-          ],
-        },
-      ],
-    },
-    orderBy: { arrivalTime: "asc" },
-  });
+  const stopTimeResponse = await prisma.$queryRaw<StopTime[]>`
+    SELECT t.trip_id,
+           t.arrival_time,
+           t.arrival_timestamp,
+           t.departure_time,
+           t.departure_timestamp,
+           t.stop_id,
+           t.stop_sequence,
+           t.stop_headsign,
+           t.pickup_type,
+           t.drop_off_type,
+           t.timepoint
+    FROM
+        stop_times AS t
+            JOIN trips ON trips.trip_id = t.trip_id
+            JOIN calendar AS c ON c.service_id = trips.service_id                     
+          left JOIN
+            calendar_dates 
+            ON trips.service_id = calendar_dates.service_id 
+            AND calendar_dates.date = ${dateOfYear}
+    WHERE
+        t.stop_id = ${stopId} AND
+        t.arrival_timestamp > ${departureTimeInSeconds}
+      AND 
+        (
+          exception_type IS NULL
+          AND ${calendarDayColumns[dayOfWeek]} = ${scheduledService.isTrue} 
+          AND ${dateOfYear} BETWEEN c.start_date AND c.end_date 
+          OR exception_type =  ${serviceException.added}
+        )
+    ORDER BY t.arrival_timestamp ASC;
+  `;
+
+  const stopTimes: StopTime[] = camelcaseKeys(stopTimeResponse);
 
   const trips = await prisma.trip.findMany({
     where: {

922f645d949de97176712baafc8dd7d3a980eb44
David Abell <79927957+david-abell@users.noreply.github.com>
2023-06-20 09:27:48 +0100

    remove commented code

--- src/pages/api/gtfs/static/static.ts
+++ src/pages/api/gtfs/static/static.ts
@@ -2,11 +2,10 @@ import { prisma } from "@/lib/db";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import withErrorHandler from "@/lib/withErrorHandler";
-import { Stop, Trip } from "@prisma/client";
+import { Stop } from "@prisma/client";
 import { ApiError } from "next/dist/server/api-utils";
 
 export type StaticAPIResponse = Stop[];
-// trips: Trip[];
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<StaticAPIResponse>
@@ -16,8 +15,6 @@ async function handler(
   if (!routeId || typeof routeId !== "string") {
     return res.end();
   }
-
-  // const trips = await prisma.trip.findMany({ where: { routeId } });
 
   const stops = await prisma.stop.findMany({
     where: { stopTime: { some: { trip: { routeId: routeId } } } },

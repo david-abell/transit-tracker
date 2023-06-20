a870e3b464c7d30aa4fee829858113f1f3e105d1
David Abell <79927957+david-abell@users.noreply.github.com>
2023-06-20 13:27:05 +0100

    remove unused config

--- src/lib/redis/createRedisInstance.ts
+++ src/lib/redis/createRedisInstance.ts
@@ -1,50 +1,43 @@
 import Redis, { RedisOptions } from "ioredis";
-import { configuration } from "./configuration";
 
-function getRedisConfiguration() {
-  return configuration.redis;
-}
+const options: RedisOptions = {
+  enableAutoPipelining: true,
+  family: process.env.NODE_ENV === "production" ? 6 : 4,
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  lazyConnect: true,
+  maxRetriesPerRequest: 0,
+  port: 6379,
+  password: process.env.REDIS_PASSWORD || "",
+  showFriendlyErrorStack: true,
+  retryStrategy: (times: number) => {
+    if (times > 3) {
+      throw new Error(
+        `[Redis] Could not connect after ${times} attempts. ${JSON.stringify(
+          options
+        )}`
+      );
+    }
 
-export function createRedisInstance(config = getRedisConfiguration()) {
+    return Math.min(times * 200, 1000);
+  },
+};
+
+export function createRedisInstance() {
   try {
-    const options: RedisOptions = {
-      enableAutoPipelining: true,
-      family: 4,
-      host: config.host || "127.0.0.1",
-      lazyConnect: true,
-      maxRetriesPerRequest: 0,
-      port: 6379,
-      showFriendlyErrorStack: true,
-      retryStrategy: (times: number) => {
-        if (times > 3) {
-          throw new Error(
-            `[Redis] Could not connect after ${times} attempts. ${JSON.stringify(
-              options
-            )}`
-          );
-        }
-
-        return Math.min(times * 200, 1000);
-      },
-    };
-
-    // if (config.port) {
-    //   options.port = config.port || 6379;
-    // }
-
-    // if (config.password) {
-    //   options.password = config.password || "";
-    // }
-
     const redis = new Redis(options);
 
+    redis.on("connect", () => console.log("Redis is connect"));
+    redis.on("reconnecting", () => console.log("Redis is reconnecting"));
+    redis.on("ready", () => console.log("Redis is ready"));
     redis.on("error", (error: unknown) => {
       console.warn("[Redis] Error connecting", error);
     });
 
     return redis;
   } catch (e) {
-    console.error(`[Redis] Could not create a Redis instance`);
-    // throw new Error(`[Redis] Could not create a Redis instance`);
+    console.error(
+      `[Redis] Could not create a Redis instance with options: ${options}`
+    );
+    throw new Error(`[Redis] Could not create a Redis instance`);
   }
 }

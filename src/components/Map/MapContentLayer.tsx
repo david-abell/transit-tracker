8de70df9ceea4840b3a1a2d217bebdaeed887291
David Abell <79927957+david-abell@users.noreply.github.com>
2023-06-30 20:22:43 +0100

    fix: zoom to selected stop

--- src/components/Map/MapContentLayer.tsx
+++ src/components/Map/MapContentLayer.tsx
@@ -90,14 +90,20 @@ function MapContentLayer({
   };
 
   useEffect(() => {
-    if (!stopIds || !stopIds.length || isEqual(stopIds, previousStopIds)) {
+    if (stopIds.length && isEqual(stopIds, previousStopIds)) {
       return;
     }
 
     const group = markerGroupRef.current;
-    if (!group || !group.getBounds().isValid()) return;
+    if (!group || !group.getBounds().isValid()) {
+      return;
+    }
 
-    map.flyToBounds(group.getBounds());
+    if (stopIds.length) {
+      map.flyToBounds(group.getBounds());
+    } else {
+      map.flyToBounds(group.getBounds(), { maxZoom: 14 });
+    }
   }, [map, stopIds, previousStopIds, selectedStopId]);
 
   useEffect(() => {
@@ -233,7 +239,7 @@ function MapContentLayer({
                       <button
                         type="button"
                         onClick={() => handleSelectedStop(stopId)}
-                        className="mx-auto my-2 block rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800
+                        className="mx-auto my-2 block w-full rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800
                          focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700
                           dark:focus:ring-blue-800"
                       >
@@ -242,7 +248,7 @@ function MapContentLayer({
                       <button
                         type="button"
                         onClick={() => addStopToSaved(stopId, stopName)}
-                        className="mx-auto my-2 flex flex-row gap-1 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800
+                        className="mx-auto my-2 flex w-full flex-row justify-center gap-1 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800
                          focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700
                           dark:focus:ring-blue-800"
                       >
@@ -256,6 +262,15 @@ function MapContentLayer({
                           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                         </svg>
                         <span>Save stop</span>
+                        <svg
+                          aria-hidden="true"
+                          className="inline-block h-5 w-5 text-yellow-400"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        </svg>
                       </button>
                     </Popup>
                   </Marker>

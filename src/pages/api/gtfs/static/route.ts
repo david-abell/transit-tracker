c57dc17e6f87ecb9a732ebb178e6775aea8e7736
David Abell <79927957+david-abell@users.noreply.github.com>
2023-07-01 09:52:50 +0100

    fix: prioritize order by routeShortName

--- src/pages/api/gtfs/static/route.ts
+++ src/pages/api/gtfs/static/route.ts
@@ -4,6 +4,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import withErrorHandler from "@/lib/withErrorHandler";
 import { Route } from "@prisma/client";
 import { ApiError } from "next/dist/server/api-utils";
+import camelcaseKeys from "camelcase-keys";
 
 export type RouteAPIResponse = Route[];
 async function handler(
@@ -16,21 +17,32 @@ async function handler(
     return res.end();
   }
 
-  const routes = await prisma.route.findMany({
-    take: 10,
-    where: {
-      OR: [
-        { routeShortName: { contains: routeName } },
-        { routeLongName: { contains: routeName } },
-      ],
-    },
-  });
+  const likeShortNameQuery = `${routeName}%`;
+  const likeLongNameQuery = `%${routeName}%`;
+  const globQuery = `${routeName}[a-zA-Z]`;
+
+  const routes = await prisma.$queryRaw<Route[]>`
+    SELECT r.route_short_name,
+          r.route_long_name,
+          r.agency_id,
+          r.route_id,
+          r.route_type
+    FROM route r
+    WHERE route_short_name LIKE ${likeShortNameQuery} OR 
+          route_long_name LIKE ${likeLongNameQuery}
+    ORDER BY route_short_name = ${routeName} DESC,
+          CAST (route_short_name AS INTEGER) ASC,
+          route_short_name GLOB ${globQuery} DESC
+    LIMIT 10;
+ `;
 
   if (!routes.length) {
     return res.end();
   }
 
-  return res.json(routes);
+  camelcaseKeys;
+
+  return res.json(camelcaseKeys(routes));
 }
 
 export default withErrorHandler(handler);

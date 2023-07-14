21ee532909d286dec5372a114a9196c08b58f906
David Abell <79927957+david-abell@users.noreply.github.com>
2023-07-14 12:12:22 +0100

    animate modal open/close

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, createContext } from "react";
+import { useEffect, useRef, createContext, useCallback } from "react";
 import { trapKeyboardFocus } from "@/lib/trapKeyboardFocus";
 
 type Props = {
@@ -28,17 +28,23 @@ function Modal({ isOpen, children, title, onProceed, onClose }: Props) {
       dialog.showModal();
       document.body.classList.add("modal-open"); // prevent bg scroll
     } else {
-      dialog.close();
-      document.body.classList.remove("modal-open");
+      // wait for close animation
+      setTimeout(() => {
+        dialog.close();
+        document.body.classList.remove("modal-open");
+      }, 200);
     }
-    return () => {
-      dialog.close();
-      document.body.classList.remove("modal-open");
-    };
   }, [isOpen]);
 
-  const proceedAndClose = () => {
+  const handleProceed = () => {
     onProceed();
+    handleClose();
+  };
+
+  const handleClose = () => {
+    const dialog = ref.current;
+    if (!dialog) return;
+
     onClose();
   };
 
@@ -55,13 +61,17 @@ function Modal({ isOpen, children, title, onProceed, onClose }: Props) {
   return (
     <dialog
       ref={ref}
-      onCancel={onClose}
-      onClick={onClose}
-      className="h-[100svh] max-h-[37.5rem] w-full max-w-3xl overflow-hidden bg-slate-50 p-0 dark:bg-gray-800 dark:text-white md:rounded-lg"
+      onCancel={handleClose}
+      onClick={handleClose}
+      data-state={isOpen ? "open" : "closed"}
+      className="h-[100svh] max-h-[37.5rem] w-full max-w-3xl overflow-hidden bg-slate-50 p-0 
+      data-[state=closed]:animate-[dialog-content-hide_200ms_forwards] data-[state=open]:animate-[dialog-content-show_200ms_forwards] 
+      backdrop:data-[state=closed]:animate-[dialog-overlay-hide_200ms_forwards] backdrop:data-[state=open]:animate-[dialog-overlay-show_200ms_forwards] dark:bg-gray-800  dark:text-white
+      md:rounded-lg"
     >
       <div
         onClick={preventAutoClose}
-        className="flex h-full flex-col justify-between gap-4 p-2 md:gap-6  md:p-6"
+        className="flex h-full flex-col justify-between gap-4 p-2 md:gap-6 md:p-6"
       >
         <h3 className="text-center text-2xl font-extrabold md:text-4xl">
           {title}
@@ -71,14 +81,14 @@ function Modal({ isOpen, children, title, onProceed, onClose }: Props) {
         </DialogRefContext.Provider>
         <div className="flex justify-between gap-3">
           <button
-            onClick={onClose}
+            onClick={handleClose}
             onKeyDown={handleKeydown}
             className="mb-2 mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Close
           </button>
           <button
-            onClick={proceedAndClose}
+            onClick={handleProceed}
             onKeyDown={handleKeydown}
             className="mb-2 mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >

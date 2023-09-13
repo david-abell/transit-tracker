71da1cfb40b629253303a6821d6a6731dc765519
David Abell <david.abell.m@gmail.com>
2023-09-13 08:01:55 +0100

    remove unused script code

--- .github/scripts/getArtifact.cjs
+++ .github/scripts/getArtifact.cjs
@@ -2,43 +2,10 @@ module.exports = async ({ github, context, core }) => {
   const owner = context.repo.owner;
   const repo = context.repo.repo;
 
-  // const workflows = await github.rest.actions.listRepoWorkflows({
-  //   owner,
-  //   repo,
-  // });
-
   const artifacts = await github.rest.actions.listArtifactsForRepo({
     owner,
     repo,
   });
-
-  // const workflow = workflows.data.workflows.find((w) =>
-  //   w.path.includes(process.env.WORKFLOW_FILENAME)
-  // );
-
-  // if (!workflow) {
-  //   core.setFailed("No workflow found");
-  //   return;
-  // }
-
-  // const runs = await github.rest.actions.listWorkflowRuns({
-  //   owner,
-  //   repo,
-  //   workflow_id: workflow.id,
-  //   status: "success",
-  //   per_page: 1,
-  // });
-
-  // if (runs.data.total_count === 0) {
-  //   core.setFailed("No runs found");
-  //   return;
-  // }
-
-  // const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
-  //   owner,
-  //   repo,
-  //   run_id: runs.data.workflow_runs[0].id,
-  // });
 
   const artifact = artifacts.data.artifacts.find(
     (artifact) => artifact.name === process.env.ARTIFACT_NAME

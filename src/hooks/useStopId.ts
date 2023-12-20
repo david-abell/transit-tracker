c2620872d666dcf192820edade3238a86f6d7475
David Abell <79927957+david-abell@users.noreply.github.com>
2023-12-20 15:26:14 +0000

    update useStopId hook fn name

--- src/hooks/useStopId.ts
+++ src/hooks/useStopId.ts
@@ -4,7 +4,7 @@ import { Stop } from "@prisma/client";
 import { fetchHelper } from "@/lib/FetchHelper";
 import { skipRevalidationOptions } from "@/lib/api/static/consts";
 
-function useStop(stopId: string) {
+function useStopId(stopId: string) {
   const { data: selectedStop } = useSWR<Stop>(
     () => (!!stopId ? `/api/gtfs/static/stops/${stopId}` : null),
     fetchHelper,
@@ -16,4 +16,4 @@ function useStop(stopId: string) {
   };
 }
 
-export default useStop;
+export default useStopId;

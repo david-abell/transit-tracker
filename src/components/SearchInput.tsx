5ca40d556db5633dd7f56b5c2a7b5d8c63f15df6
David Abell <79927957+david-abell@users.noreply.github.com>
2023-12-21 12:16:33 +0000

    fix: limit max results height

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -143,7 +143,7 @@ function SearchInput({ selectedRoute, className = "" }: Props) {
 
           {!!searchResults.length && (
             <ul
-              className="text-md absolute left-0 top-full z-[1200] block w-full rounded-b-lg border border-gray-300 bg-gray-50 text-gray-900
+              className="text-md absolute left-0 top-full z-[1200] block max-h-[calc(100dvh-6rem)] w-full overflow-y-scroll rounded-b-lg border border-gray-300 bg-gray-50 text-gray-900
                focus:border-blue-500  focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             >
               {searchResults.map((item) => {

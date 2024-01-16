131348849830de938aedf2e101b89479fa8f825e
David Abell <79927957+david-abell@users.noreply.github.com>
2024-01-16 16:50:23 +0000

    fix: improved error boundary text

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -17,16 +17,22 @@ class ErrorBoundary extends Component {
     // You can use your own error logging service here
     console.log({ error, errorInfo });
   }
+
+  handleOnClick() {
+    window.location.href = location.origin;
+  }
+
   render() {
     // Check if the error is thrown
     if (this.state.hasError) {
       // You can render any custom fallback UI
       return (
-        <div>
-          <h2>Oops, there is an error!</h2>
+        <div className="flex h-[100dvh] flex-col items-center justify-center bg-gray-50 text-xl text-slate-950 dark:bg-gray-800 dark:text-white">
+          <h2>Oops, there was an error!</h2>
           <button
             type="button"
-            onClick={() => this.setState({ hasError: false })}
+            className="my-4 me-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            onClick={() => this.handleOnClick()}
           >
             Try again?
           </button>

c917f033b71c1258e09f6306992e1fc8082054fa
David Abell <79927957+david-abell@users.noreply.github.com>
2024-01-19 15:34:23 +0000

    fix: simplify upcoming query logic to one db query done by dialog

--- src/hooks/useUpcoming.ts
+++ src/hooks/useUpcoming.ts
@@ -2,12 +2,12 @@ import useSWR from "swr";
 import { Route, Trip } from "@prisma/client";
 
 import { fetchHelper } from "@/lib/FetchHelper";
-import { TripsByStopIdAPIResponse } from "@/pages/api/gtfs/static/upcoming";
+import { UpcomingTripsAPIResponse } from "@/pages/api/gtfs/static/upcoming";
 import { skipRevalidationOptions } from "@/lib/api/static/consts";
 import { ApiError } from "next/dist/server/api-utils";
 
 function useTrips(stopId: string | null, selectedDateTime: string, page = 0) {
-  const { data, error, isLoading } = useSWR<TripsByStopIdAPIResponse, ApiError>(
+  const { data, error, isLoading } = useSWR<UpcomingTripsAPIResponse, ApiError>(
     () =>
       !!stopId
         ? `/api/gtfs/static/upcoming?${new URLSearchParams({
@@ -20,34 +20,10 @@ function useTrips(stopId: string | null, selectedDateTime: string, page = 0) {
     skipRevalidationOptions
   );
 
-  const { routes: routeData, stopTimes, trips: tripData } = data || {};
-
-  const routes: Map<string, Route> =
-    routeData && routeData.length
-      ? new Map(
-          routeData?.map((route) => {
-            const { routeId } = route;
-            return [routeId, route];
-          })
-        )
-      : new Map();
-
-  const trips: Map<string, Trip> =
-    tripData && tripData.length
-      ? new Map(
-          tripData?.map((trip) => {
-            const { tripId } = trip;
-            return [tripId, trip];
-          })
-        )
-      : new Map();
-
   return {
     error,
     isLoading,
-    routes,
-    trips,
-    stopTimes,
+    upcomingTrips: data?.upcomingTrips,
   };
 }
 

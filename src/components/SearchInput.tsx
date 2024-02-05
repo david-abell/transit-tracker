63a7d8e860ace811526e0e138bf7886a7ca5df09
David Abell <79927957+david-abell@users.noreply.github.com>
2024-02-05 15:02:30 +0000

    refactor search input to use cn utility

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -5,6 +5,8 @@ import { trapKeyboardFocus } from "@/lib/trapKeyboardFocus";
 import { useRouter } from "next/router";
 import useStops from "@/hooks/useStops";
 
+import { cn } from "@/lib/utils";
+
 type Props = {
   selectedRoute: Route | undefined;
   className?: string;
@@ -91,97 +93,98 @@ function SearchInput({ selectedRoute, className = "" }: Props) {
   // };
 
   return (
-    <div className={className}>
-      <form
-        ref={formRef}
-        className="flex flex-1 flex-col items-center justify-center text-center lg:min-w-[32rem]"
-        // onSubmit={handleOnSubmit}
-      >
-        <label htmlFor="search-select" className="sr-only">
-          Search for a transport route or stop
-        </label>
-        <div className="relative flex w-full">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2 md:pl-2.5">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 text-gray-500 dark:text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </div>
+    <form
+      ref={formRef}
+      className={cn(
+        "flex flex-1 flex-col items-center justify-center text-center w-full",
+        className,
+      )}
+      // onSubmit={handleOnSubmit}
+    >
+      <label htmlFor="search-select" className="sr-only">
+        Search for a transport route or stop
+      </label>
+      <div className="relative flex w-full">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2 md:pl-2.5">
+          <svg
+            aria-hidden="true"
+            className="h-5 w-5 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </div>
 
-          <input
-            id="search-select"
-            className={`h-10 text-md inline-block w-full appearance-none truncate rounded-none rounded-b-lg rounded-t-lg border border-gray-300 p-2.5 pl-8
+        <input
+          id="search-select"
+          className={`h-10 text-md inline-block w-full appearance-none truncate rounded-none rounded-b-lg rounded-t-lg border border-gray-300 p-2.5 pl-8
                text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:focus:border-blue-500 dark:focus:ring-blue-500 md:pl-10
                ${
                  searchQuery ? "rounded-b-none focus-within:rounded-b-none" : ""
                }`}
-            placeholder={
-              selectedRoute?.routeLongName ||
-              "Search for a vehicle route or stop"
-            }
-            type="search"
-            value={searchQuery}
-            onChange={handleSearchChange}
-            autoComplete="off"
-            aria-autocomplete="list"
-            onKeyDown={(e) => handleSearchKeydown(e)}
-          />
+          placeholder={
+            selectedRoute?.routeLongName || "Search for a vehicle route or stop"
+          }
+          type="search"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          autoComplete="off"
+          aria-autocomplete="list"
+          onKeyDown={(e) => handleSearchKeydown(e)}
+        />
 
-          {!!searchResults.length && (
-            <ul
-              className="text-md absolute left-0 top-full z-[1200] block max-h-[calc(100dvh-6rem)] w-[calc(100dvw-2rem)] bg-popover overflow-y-scroll rounded-b-lg border border-gray-300 focus:border-blue-500
+        {!!searchResults.length && (
+          <ul
+            className="text-md absolute left-0 top-full z-[1200] block max-h-[calc(100dvh-6rem)] w-[calc(100dvw-2rem)] bg-popover overflow-y-scroll rounded-b-lg border border-gray-300 focus:border-blue-500
                focus:ring-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 md:w-full"
-            >
-              {searchResults.map((item) => {
-                if ("routeId" in item) {
-                  const { routeId, routeLongName, routeShortName } = item;
-                  return (
-                    <li
-                      key={routeId + routeLongName}
-                      className="pb-2 pl-10 pr-2.5  hover:bg-slate-200"
+          >
+            {searchResults.map((item) => {
+              if ("routeId" in item) {
+                const { routeId, routeLongName, routeShortName } = item;
+                return (
+                  <li
+                    key={routeId + routeLongName}
+                    className="pb-2 pl-10 pr-2.5  hover:bg-slate-200"
+                  >
+                    <button
+                      type="button"
+                      onClick={(e) => handleSearchSelection(e, item)}
+                      className="w-full text-left"
+                      onKeyDown={(e) => handleSearchKeydown(e)}
                     >
-                      <button
-                        type="button"
-                        onClick={(e) => handleSearchSelection(e, item)}
-                        className="w-full text-left"
-                        onKeyDown={(e) => handleSearchKeydown(e)}
-                      >
-                        <strong>Route: </strong>
-                        {routeShortName} - {routeLongName}
-                      </button>
-                    </li>
-                  );
-                } else {
-                  const { stopId, stopCode, stopName } = item;
-                  return (
-                    <li
-                      key={stopId + stopName}
-                      className="pb-2 pl-10 pr-2.5  hover:bg-slate-200"
+                      <strong>Route: </strong>
+                      {routeShortName} - {routeLongName}
+                    </button>
+                  </li>
+                );
+              } else {
+                const { stopId, stopCode, stopName } = item;
+                return (
+                  <li
+                    key={stopId + stopName}
+                    className="pb-2 pl-10 pr-2.5  hover:bg-slate-200"
+                  >
+                    <button
+                      type="button"
+                      onClick={(e) => handleSearchSelection(e, item)}
+                      className="w-full text-left"
+                      onKeyDown={(e) => handleSearchKeydown(e)}
                     >
-                      <button
-                        type="button"
-                        onClick={(e) => handleSearchSelection(e, item)}
-                        className="w-full text-left"
-                        onKeyDown={(e) => handleSearchKeydown(e)}
-                      >
-                        <strong>Stop:</strong> {stopCode ?? stopId} - {stopName}
-                      </button>
-                    </li>
-                  );
-                }
-              })}
-            </ul>
-          )}
-          {/* <button
+                      <strong>Stop:</strong> {stopCode ?? stopId} - {stopName}
+                    </button>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        )}
+        {/* <button
             // onClick={handleOnSubmit}
             onKeyDown={(e) => handleSearchKeydown(e)}
             type="submit"
@@ -205,9 +208,8 @@ function SearchInput({ selectedRoute, className = "" }: Props) {
             </svg>
             <span className="sr-only">Search</span>
           </button> */}
-        </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 }
 

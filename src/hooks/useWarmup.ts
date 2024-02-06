0aab0eed7c307dab2d2468b1d0dc71486f79722b
David Abell <79927957+david-abell@users.noreply.github.com>
2024-02-06 08:22:32 +0000

    revert deleting useWarmup

---
+++ src/hooks/useWarmup.ts
@@ -0,0 +1,17 @@
+import useSWR from "swr";
+
+import { ApiError } from "next/dist/server/api-utils";
+import { StopAPIResponse } from "@/pages/api/gtfs/static/stops/[stopId]";
+
+function useWarmup() {
+  const { error, isLoading } = useSWR<StopAPIResponse, ApiError>(
+    "/api/gtfs/static/stops/8220B1351201",
+  );
+
+  return {
+    error,
+    isLoading,
+  };
+}
+
+export default useWarmup;

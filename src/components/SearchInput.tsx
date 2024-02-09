87c416ff189cc315872859de3c3bf6d2567f1869
David Abell <79927957+david-abell@users.noreply.github.com>
2024-02-09 15:58:30 +0000

    refactor SearchInput to use React-select

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,15 @@
 import useRoute from "@/hooks/useRoute";
 import { Route, Stop } from "@prisma/client";
-import { useState, useRef, useEffect, useCallback } from "react";
-import { trapKeyboardFocus } from "@/lib/trapKeyboardFocus";
-import { useRouter } from "next/router";
+import { useState, useMemo, CSSProperties } from "react";
 import useStops from "@/hooks/useStops";
+import Select, { InputActionMeta } from "react-select";
 
-import { cn } from "@/lib/utils";
-import { parseAsString, useQueryState } from "nuqs";
+import { useQueryState } from "nuqs";
+
+export interface GroupedOption {
+  readonly label: string;
+  readonly options: readonly Stop[] | readonly Route[];
+}
 
 type Props = {
   selectedRoute: Route | undefined;
@@ -23,205 +26,156 @@ function SearchInput({
 }: Props) {
   const [, setRouteId] = useQueryState("routeId");
 
-  const [searchQuery, setSearchQuery] = useState("");
-  const { routes } = useRoute(searchQuery);
-  const { stops } = useStops({ stopQuery: searchQuery });
-  const formRef = useRef<HTMLFormElement>(null);
+  const [inputText, setInputText] = useState<string>("");
+  const [selectedOption, setSelectedOption] = useState<Stop | Route | null>(
+    null,
+  );
 
-  const handleSearchSelection = (
-    e: React.MouseEvent<HTMLButtonElement>,
-    query: Route | Stop,
-  ) => {
-    e.stopPropagation();
+  // still needs debouncing
+  const [searchText, setSearchText] = useState<string>("");
 
-    if ("stopId" in query) {
-      removeQueryParams();
-      setStopId(query.stopId);
+  const { routes } = useRoute(searchText);
+  const { stops } = useStops({ stopQuery: searchText });
+
+  const searchResults: GroupedOption[] = useMemo(() => {
+    return [
+      { label: "Routes", options: routes ?? [] },
+      { label: "Stops", options: stops ?? [] },
+    ];
+  }, [routes, stops]);
+
+  const handleOptionLabel = (item: Stop | Route) => {
+    if ("routeId" in item) {
+      const { routeLongName, routeShortName } = item;
+
+      return `Route: ${routeShortName} - ${routeLongName}`;
+    } else {
+      const { stopId, stopCode, stopName } = item;
+
+      return `Stop: ${stopCode ?? stopId} - ${stopName}`;
     }
-    if ("routeId" in query) {
-      removeQueryParams();
-      setRouteId(query.routeId);
-    }
-    setSearchQuery("");
   };
 
-  // trap keyboard focus inside form for arrow and tab key input
-  const handleSearchKeydown = (
-    e: React.KeyboardEvent<
-      HTMLButtonElement | HTMLInputElement | HTMLAnchorElement
-    >,
-  ) => {
-    if (!formRef.current) return;
-    if (e.key === "Escape") {
-      e.preventDefault();
-      setSearchQuery("");
-      return;
+  const handleOptionValue = (item: Stop | Route) => {
+    if ("stopId" in item) {
+      return item.stopId;
+    } else {
+      return item.routeId;
     }
-    trapKeyboardFocus(e, formRef.current);
   };
 
-  useEffect(() => {
-    const ref = formRef.current;
+  const handleSelectionValue = (option: Stop | Route | null) => {
+    if (!option) return;
+    removeQueryParams();
+    setSelectedOption(option);
 
-    if (!ref) return;
-
-    const handleClickOutside = (event: MouseEvent) => {
-      if (ref && !ref.contains(event.target as Node)) {
-        setSearchQuery("");
-      }
-    };
-    document.addEventListener("click", handleClickOutside, true);
-
-    return () => {
-      document.removeEventListener("click", handleClickOutside, true);
-    };
-  }, []);
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value);
+    if ("stopId" in option) {
+      setStopId(option.stopId);
+    } else {
+      setRouteId(option.routeId);
+    }
   };
 
-  const isRoutes = !!routes && routes.length > 0;
-  const isStops = !!stops && stops.length > 0;
+  const handleInputChange = (inputText: string, meta: InputActionMeta) => {
+    if (meta.action === "input-change") {
+      setInputText(inputText);
+      setSearchText(inputText);
+    }
+  };
 
-  const searchResults =
-    isRoutes && isStops
-      ? [...routes, ...stops]
-      : isRoutes
-        ? routes
-        : isStops
-          ? stops
-          : [];
-
-  // onsubmit not working
-
-  // const handleOnSubmit = (
-  //   e: React.FormEvent<HTMLFormElement | HTMLButtonElement>
-  // ) => {
-  //   e.preventDefault();
-  //   router.push({ pathname: "/", query: { routeId } });
-  //   setSearchQuery("");
-  // };
+  console.log({ inputText, searchText, routes, stops });
 
   return (
-    <form
-      ref={formRef}
-      className={cn(
-        "z-[1300] flex flex-1 flex-col items-center justify-center text-center w-full",
-        className,
-      )}
-      // onSubmit={handleOnSubmit}
+    <>
+      <Select<Readonly<Stop> | Readonly<Route>, false, GroupedOption>
+        components={{
+          DropdownIndicator: () => <SearchIcon />,
+          IndicatorSeparator: () => null,
+        }}
+        options={searchResults}
+        onInputChange={handleInputChange}
+        onChange={handleSelectionValue}
+        value={selectedOption}
+        getOptionLabel={handleOptionLabel}
+        getOptionValue={handleOptionValue}
+        formatGroupLabel={GroupLabel}
+        formatOptionLabel={formatOptionLabel}
+        className="z-[1300] min-w-[8rem] w-full"
+        placeholder={"Search for a transport route or stop"}
+        styles={selectStyles}
+      ></Select>
+    </>
+  );
+}
+
+const formatOptionLabel = (item: Stop | Route) => {
+  if ("routeId" in item) {
+    const { routeLongName, routeShortName } = item;
+    return (
+      <span>
+        <b>{routeShortName ?? ""}:</b> {routeLongName ?? "unknown"}
+      </span>
+    );
+  } else {
+    const { stopId, stopCode, stopName } = item;
+    return (
+      <span>
+        <b>{stopCode ?? stopId}:</b> {stopName ?? "unknown"}
+      </span>
+    );
+  }
+};
+
+const SearchIcon = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      className="h-5 w-5 text-gray-500 inline-block"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <label htmlFor="search-select" className="sr-only">
-        Search for a transport route or stop
-      </label>
-      <div className="relative flex w-full">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2 md:pl-2.5">
-          <svg
-            aria-hidden="true"
-            className="h-5 w-5 text-gray-500 dark:text-gray-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </div>
+      <path
+        fillRule="evenodd"
+        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+};
 
-        <input
-          id="search-select"
-          className={`text-md appearance-none truncate rounded-b-lg rounded-t-lg p-2.5 pl-8 md:pl-10
-               h-10 w-full rounded-md border border-input bg-popover text-md ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1
-               ${
-                 searchQuery ? "rounded-b-none focus-within:rounded-b-none" : ""
-               }`}
-          placeholder={
-            selectedRoute?.routeLongName || "Search for a vehicle route or stop"
-          }
-          type="search"
-          value={searchQuery}
-          onChange={handleSearchChange}
-          autoComplete="off"
-          aria-autocomplete="list"
-          onKeyDown={(e) => handleSearchKeydown(e)}
-        />
-
-        {!!searchResults.length && (
-          <ul
-            className="text-md absolute left-0 top-full block max-h-[calc(100dvh-6rem)] w-[calc(100dvw-2rem)] bg-popover overflow-y-scroll rounded-b-lg border border-gray-300 focus:border-blue-500
-               focus:ring-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 md:w-full"
-          >
-            {searchResults.map((item) => {
-              if ("routeId" in item) {
-                const { routeId, routeLongName, routeShortName } = item;
-                return (
-                  <li
-                    key={routeId + routeLongName}
-                    className="pb-2 pl-10 pr-2.5  hover:bg-slate-200"
-                  >
-                    <button
-                      type="button"
-                      onClick={(e) => handleSearchSelection(e, item)}
-                      className="w-full text-left"
-                      onKeyDown={(e) => handleSearchKeydown(e)}
-                    >
-                      <strong>Route: </strong>
-                      {routeShortName} - {routeLongName}
-                    </button>
-                  </li>
-                );
-              } else {
-                const { stopId, stopCode, stopName } = item;
-                return (
-                  <li
-                    key={stopId + stopName}
-                    className="pb-2 pl-10 pr-2.5  hover:bg-slate-200"
-                  >
-                    <button
-                      type="button"
-                      onClick={(e) => handleSearchSelection(e, item)}
-                      className="w-full text-left"
-                      onKeyDown={(e) => handleSearchKeydown(e)}
-                    >
-                      <strong>Stop:</strong> {stopCode ?? stopId} - {stopName}
-                    </button>
-                  </li>
-                );
-              }
-            })}
-          </ul>
-        )}
-        {/* <button
-            // onClick={handleOnSubmit}
-            onKeyDown={(e) => handleSearchKeydown(e)}
-            type="submit"
-            className={`text-md flex w-12 justify-center rounded-br-lg rounded-tr-lg border border-blue-700 bg-blue-700 p-2.5 font-medium text-white hover:bg-blue-800 
-              focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
-              ${searchQuery ? "rounded-br-none" : ""} `}
-          >
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
-            <span className="sr-only">Search</span>
-          </button> */}
-      </div>
-    </form>
+function GroupLabel(data: GroupedOption) {
+  return (
+    <div style={groupStyles}>
+      <span>{data.label}</span>
+    </div>
   );
 }
 
 export default SearchInput;
+
+const selectStyles = {
+  control: (base: any) => ({
+    ...base,
+    flexDirection: "row-reverse",
+  }),
+  clearIndicator: (base: any) => ({
+    ...base,
+    position: "absolute",
+    right: 0,
+  }),
+  indicatorsContainer: (base: any) => ({
+    ...base,
+    paddingLeft: "8px",
+  }),
+  menuList: (base: any) => ({
+    ...base,
+    minHeight: "400px", // your desired height
+  }),
+};
+
+const groupStyles: CSSProperties = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+};

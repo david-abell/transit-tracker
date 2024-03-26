843f3463049199e072d7e1138a439b1412a4350f
David Abell <79927957+david-abell@users.noreply.github.com>
2024-03-26 11:20:29 +0000

    fix: clarify popup button text

--- src/components/Map/MapContentLayer.tsx
+++ src/components/Map/MapContentLayer.tsx
@@ -331,7 +331,7 @@ function MapContentLayer({
                     )}
                     <div className="flex flex-col gap-2 mt-4">
                       <Button onClick={() => handleSelectedStop(stopId)}>
-                        Upcoming trips
+                        View trips
                       </Button>
                       {!!selectedStopId && isValidDestination && (
                         <Button onClick={() => handleDestinationStop(stopId)}>
@@ -351,7 +351,7 @@ function MapContentLayer({
                         >
                           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                         </svg>
-                        <span>save to favourites</span>
+                        <span>Favourite</span>
                         <svg
                           aria-hidden="true"
                           className="inline-block h-5 w-5 text-yellow-400"

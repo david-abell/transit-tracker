38c786e0e0fed243e2a1985ddb42c78fb2b87627
David Abell <79927957+david-abell@users.noreply.github.com>
2024-04-16 17:54:00 +0100

    add leaflet edgebuffer to preload offscreen tiles

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,6 +4,7 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import { LatLngTuple } from "leaflet";
 import MapContentLayer from "./MapContentLayer";
 import L from "leaflet";
+import "leaflet-edgebuffer";
 
 export const canvasRenderer = L.canvas();
 
@@ -11,6 +12,13 @@ type MapContentLayerProps = React.ComponentProps<typeof MapContentLayer>;
 
 interface Props extends MapContentLayerProps {
   center: LatLngTuple;
+}
+
+// leaflet-edgebuffer has no type definition. Declare it's options here.
+declare module "react-leaflet" {
+  export interface TileLayerProps {
+    edgeBufferTiles: number;
+  }
 }
 
 export const MAP_DEFAULT_ZOOM = 13;
@@ -29,6 +37,10 @@ function Map({ center, ...props }: Props) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         className="map-tiles"
+        detectRetina
+        edgeBufferTiles={1}
+        keepBuffer={1}
+        updateWhenIdle={false}
       />
       <MapContentLayer center={center} {...props} />
     </MapContainer>

2c21d7783ca80e9b37bc746df2498dcf4f2933b2
David Abell <79927957+david-abell@users.noreply.github.com>
2024-04-22 13:21:15 +0100

    update LiveText caution text color

--- src/components/LiveText.tsx
+++ src/components/LiveText.tsx
@@ -1,22 +1,37 @@
 import { useState } from "react";
 import { useInterval } from "usehooks-ts";
+import LiveMarkerTooltip from "./Map/LiveMarkerTooltip";
+import LiveVehicleTooltip from "./Map/LiveVehicleTooltip";
+import { cn } from "@/lib/utils";
 
 export type LiveTextColor = "alert" | "caution" | "info" | "default";
 
 type Props = {
+  className?: string;
   content: string | (() => string);
+  contentBefore?: string;
+  contentAfter?: string;
   color?: LiveTextColor;
   delayInSeconds?: number;
+  tooltip?: "marker" | "vehicle";
 };
 
 const colors = {
   alert: "text-red-700 dark:text-red-500",
-  caution: "yellow",
+  caution: "text-yellow-700 dark:text-yellow-500",
   info: "text-green-700 dark:text-green-500",
   default: "",
 };
 
-function LiveText({ content, color = "default", delayInSeconds = 1 }: Props) {
+function LiveText({
+  className = "",
+  content,
+  contentBefore,
+  contentAfter = "",
+  color = "default",
+  delayInSeconds = 1,
+  tooltip,
+}: Props) {
   const [textContent, setTextContent] = useState(
     typeof content === "string" ? content : content(),
   );
@@ -24,7 +39,15 @@ function LiveText({ content, color = "default", delayInSeconds = 1 }: Props) {
   useInterval(() => {
     setTextContent(typeof content === "string" ? content : content());
   }, delayInSeconds * 1000);
-  return <span className={colors[color]}>{textContent}</span>;
+  return (
+    <span className={cn(className, "whitespace-nowrap text-lg ")}>
+      {!!contentBefore && <span>{contentBefore} </span>}
+      <span className={cn("font-bold", colors[color])}>{textContent}</span>
+      <span>{contentAfter} </span>
+      {textContent && tooltip === "marker" && <LiveMarkerTooltip />}
+      {textContent && tooltip === "vehicle" && <LiveVehicleTooltip />}
+    </span>
+  );
 }
 
 export default LiveText;

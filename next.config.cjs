26bcc9309bf230acd9ba9ca79c797aa4447fb4cf
David Abell <79927957+david-abell@users.noreply.github.com>
2024-05-20 12:33:44 +0100

    add noe_modules symlink config

--- next.config.cjs
+++ next.config.cjs
@@ -3,6 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["react-leaflet-cluster"],
   productionBrowserSourceMaps: true,
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+  ) => {
+    if (dev) {
+      config.watchOptions = {
+        followSymlinks: true,
+      };
+
+      config.snapshot.managedPaths = [];
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;

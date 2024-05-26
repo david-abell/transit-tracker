f30a0f2924785b51bab317eb93e4fdad44f6fb76
David Abell <david.abell.m@gmail.com>
2024-05-26 17:57:18 +0100

    fix: indent wrapped title text and list items

--- src/components/changelog/Version.tsx
+++ src/components/changelog/Version.tsx
@@ -4,15 +4,12 @@ function Version({ version, changes, title }: Changelog) {
   return (
     <section>
       <header>
-        {!!title && <p className="italic">{title}</p>}
+        {!!title && <p className="italic py-2 pl-2 -indent-2">{title}</p>}
         <h3 className="text-lg">Version {version}:</h3>
       </header>
-      <ul>
+      <ul className="list-outside list-disc pl-5">
         {changes.map((change) => (
-          <li
-            className="before:content-['-'] before:mr-2"
-            key={version + change}
-          >
+          <li className="last:pb-2" key={version + change}>
             {change}
           </li>
         ))}
